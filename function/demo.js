b82e092f0d816213bb9e936e1c9841cb9993dc11
Adesh Kanthali <92244882+Adeshkanthali@users.noreply.github.com>
2024-07-30 22:58:54 +0530

    Add files via upload

---
+++ function/demo.js
@@ -0,0 +1,24 @@
+
+//........................ways of write function in javaScript.....................//
+
+// 1st way :-
+
+function fun() {
+    console.log(20 + 20);
+}
+fun();
+
+
+// 2nd way :-
+
+function myfunction() {
+    return 12 * 5;
+}
+console.log(myfunction());
+
+
+// 3rd way :-
+
+function newfun() {
+    document.write(20 + 20);
+}
